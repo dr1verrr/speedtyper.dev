56ebf21c48ba6e72432cc1407027a574933a9034
dr1verrr <darfor2107@gmail.com>
2022-08-19 16:50:23 +0300

    set aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { firebaseConfig } from 'app/config/firebase'
+import { firebaseConfig } from '@/app/config/firebase'
 
 function App() {
   const [count, setCount] = useState(0)
