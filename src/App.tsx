78200c372552e93a1db74c46e47f81df5190b001
dr1verrr <darfor2107@gmail.com>
2022-08-23 17:40:30 +0300

    feat(App): init application Add routes, Layout wrapper, checking auth data

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
-import { useState } from 'react'
+import { auth } from '@/app/config/firebase'
+import Layout from '@/components/wrappers/Layout'
+import { authChanged } from '@/store/auth/events'
+import { useEvent } from 'effector-react'
+import { onAuthStateChanged } from 'firebase/auth'
+import { useEffect } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
-import reactLogo from './assets/react.svg'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const updateAuthData = useEvent(authChanged)
+
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      updateAuthData(!!user)
+    })
+  }, [])
 
   return (
-    <div className='App'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </div>
+    <Router>
+      <Layout>
+        <AppRoutes />
+      </Layout>
+    </Router>
   )
 }
 
