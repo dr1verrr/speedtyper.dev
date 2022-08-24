9eb3f484f619cc51c775cd86ae493f549d4f5901
dr1verrr <darfor2107@gmail.com>
2022-08-24 21:20:12 +0300

    refactor(wrappers): change theme provider

--- src/components/wrappers/Layout.tsx
+++ src/components/wrappers/Layout.tsx
@@ -1,12 +1,15 @@
-import ThemeProvider from '@/context/theme'
-import { darkTheme, lightTheme } from '@/context/theme/constants'
+import Spinner from '@/components/loaders/Spinner'
+import Box from '@/components/shared/Box'
 import useMediaQuery from '@/hooks/useMediaQuery'
+import NavBar from '@/layout/NavBar/NavBar'
+import { darkTheme, lightTheme } from '@/services/theme/themes'
 import { LocalStorageKeys } from '@/store/theme/constants'
 import { themeChanged } from '@/store/theme/events'
 import $theme, { ThemeStore } from '@/store/theme/store'
 import { loadState } from '@/utils/localStorage'
 import { useEvent, useStore } from 'effector-react'
-import { ReactNode, useEffect, useMemo, useRef, useState } from 'react'
+import { ReactNode, Suspense, useEffect, useMemo, useRef, useState } from 'react'
+import { ThemeProvider } from 'react-jss'
 import { ToastContainer } from 'react-toastify'
 
 type LayoutProps = {
@@ -64,12 +67,30 @@ export default function Layout({ children }: LayoutProps) {
 
   return (
     <ThemeProvider theme={currentTheme}>
+      <div
+        style={{
+          background: currentTheme.common.bg,
+          color: currentTheme.common.text,
+          minHeight: '100vh'
+        }}
+      >
+        <NavBar />
+        <Suspense
+          fallback={
+            <Box sx={{ position: 'fixed', bottom: 50, left: 50 }}>
+              <Spinner size={50} />
+            </Box>
+          }
+        >
+          <main style={{ position: 'relative', minHeight: 'inherit' }}>{children}</main>
+        </Suspense>
+      </div>
+
       <ToastContainer
         position='top-center'
         style={{ fontWeight: 'bold' }}
         theme={toastContainerTheme}
       />
-      {children}
     </ThemeProvider>
   )
 }
