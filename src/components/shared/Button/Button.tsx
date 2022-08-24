10bd26a9c55037ff7297e75aa31495172b3ea07e
dr1verrr <darfor2107@gmail.com>
2022-08-24 21:08:13 +0300

    refactor(shared): change props Button: Implemented jss

--- src/components/shared/Button/Button.tsx
+++ src/components/shared/Button/Button.tsx
@@ -1,28 +1,44 @@
-import { useTheme } from '@/context/theme/constants'
+import { useTheme } from '@/services/theme/actions'
+import { Theme } from '@/services/theme/types'
 import React, { ReactNode } from 'react'
+import { createUseStyles } from 'react-jss'
+
+type RuleNames = 'button'
+type Variants = 'primary' | 'action' | 'default'
 
 type ButtonProps = {
-  variant?: 'primary' | 'action'
+  variant?: Variants
   sx?: React.CSSProperties
   onClick?: React.MouseEventHandler<HTMLButtonElement>
   children?: ReactNode
 }
 
-export default function Button({
-  sx,
-  onClick,
-  children,
-  variant = 'primary'
-}: ButtonProps) {
-  const { theme } = useTheme()
-  const buttonProps = theme.button.variant[variant]
+type ButtonStyledProps = Omit<ButtonProps, 'variant'> & {
+  variant: Variants
+}
+
+const useStyles = createUseStyles<RuleNames, ButtonStyledProps, Theme>({
+  button: {
+    background: ({ theme, variant }) => theme.button.variant[variant].bg,
+    color: ({ theme, variant }) => theme.button.variant[variant].text,
+    border: 0,
+    padding: '7px 10px',
+    borderRadius: 10,
+    cursor: 'pointer'
+  }
+})
+
+export default function Button({ variant = 'default', ...props }: ButtonProps) {
+  const theme = useTheme()
+  const classes = useStyles({ ...props, variant, theme })
 
   return (
     <button
-      onClick={onClick}
-      style={{ background: buttonProps.bg, color: buttonProps.text, ...sx }}
+      className={classes.button}
+      {...props}
+      style={props.sx}
     >
-      {children}
+      {props.children}
     </button>
   )
 }
