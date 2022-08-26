fa4c959cc83b65b45a8601ce01594c4efe4f2438
dr1verrr <darfor2107@gmail.com>
2022-08-26 18:22:11 +0300

    Update Home.tsx

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,24 +1,15 @@
-import SwitchTheme from '@/components/buttons/SwitchTheme'
-import Spinner from '@/components/loaders/Spinner'
+import SpinnerWave from '@/components/loaders/SpinnerWave'
 import { Typography } from '@/components/shared'
 import Box from '@/components/shared/Box'
 import Container from '@/components/shared/Container'
-import { themeToggled } from '@/store/theme/events'
-import { useEvent } from 'effector-react'
 
 export default function Home() {
-  const toggleTheme = useEvent(themeToggled)
-
   return (
-    <div>
+    <Box>
       <Container maxWidth='md'>
-        <div>Home</div>
-        <Box sx={{ position: 'fixed', bottom: 50, left: 50 }}>
-          <Spinner size={50} />
-        </Box>
         <Typography variant='h1'>Heading</Typography>
-        <SwitchTheme onClick={toggleTheme} />
+        <SpinnerWave />
       </Container>
-    </div>
+    </Box>
   )
 }
