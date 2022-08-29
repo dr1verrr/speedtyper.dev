d4039f09e53272a0aea61cabcd6a3af044af5096
dr1verrr <darfor2107@gmail.com>
2022-08-29 09:57:33 +0300

    Update PublicRoute.tsx

--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -1,10 +1,10 @@
-import $auth from '@/store/auth/store'
 import { useStore } from 'effector-react'
 import { Navigate, Outlet } from 'react-router-dom'
 
+import $auth from '@/store/auth/store'
+
 function PublicRoute() {
   const isAuthenticated = useStore($auth)
-  console.log('Authenticated changed', isAuthenticated)
 
   return !isAuthenticated ? <Outlet /> : <Navigate to='/profile' />
 }
