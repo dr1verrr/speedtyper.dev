e037e11f56201705a6f4eab8038f3b74a1ec7a9f
dr1verrr <darfor2107@gmail.com>
2022-08-29 09:50:55 +0300

    refactor(App): style changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css'
-import Spinner from './components/loaders/Spinner'
-import Box from './components/shared/Box'
-import { auth } from '@/app/config/firebase'
-import Layout from '@/components/wrappers/Layout'
-import AppRoutes from '@/routes/AppRoutes'
-import { authChanged } from '@/store/auth/events'
 import { useEvent } from 'effector-react'
 import { onAuthStateChanged } from 'firebase/auth'
 import { Suspense, useEffect } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+
+import Spinner from './components/loaders/Spinner'
+import Box from './components/shared/Box'
+
+import { auth } from '@/app/config/firebase'
+import Layout from '@/components/wrappers/Layout'
+import AppRoutes from '@/routes/AppRoutes'
+import { authChanged } from '@/store/auth/events'
 
 function App() {
   const updateAuthData = useEvent(authChanged)
@@ -25,7 +27,7 @@ function App() {
         <Suspense
           fallback={
             <Box sx={{ position: 'fixed', bottom: 50, left: 50 }}>
-              <Spinner size={50} />
+              <Spinner size={35} />
             </Box>
           }
         >
