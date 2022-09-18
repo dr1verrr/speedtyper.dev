fde198d7e9fd8dceb33a8666a268164d4f2ad96b
dr1verrr <darfor2107@gmail.com>
2022-09-18 11:34:23 +0300

    refactor(views): improve `Home` page

---
+++ src/views/Home/constants.ts
@@ -0,0 +1,12 @@
+const videos = {
+  features: {
+    typing:
+      'https://user-images.githubusercontent.com/67706933/190857521-fa1d4098-30c0-4a6e-9d13-ef45ac70c1e5.mp4',
+    manageData:
+      'https://user-images.githubusercontent.com/67706933/190857566-f6dd8223-cbbc-4f25-bf7b-c8eba0ad5b16.mp4',
+    preferences:
+      'https://user-images.githubusercontent.com/67706933/190857577-dba442a0-de2f-4ef6-a055-a2019adf80ab.mp4'
+  }
+}
+
+export { videos }
